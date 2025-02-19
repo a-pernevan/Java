700331454b1a2bd146761ec31e49baf677c17733
Andrei Pernevan <apernevan@outlook.com>
2025-02-19 12:29:01 +0200

    java examples

---
+++ Library.java
@@ -0,0 +1,26 @@
+public class Library {
+    // attributes
+    // methods
+    String bookName = "Zero to One";
+    String authorName = "Peter Thiel";
+    int quantity = 2;
+
+    public void bookDetails() {
+        System.out.println("Book Name: " + bookName);
+        System.out.println("Author Name: " + authorName);
+    }
+
+    public int updateQuantity(int newQuantity) {
+        quantity = newQuantity;
+        return quantity;
+    }
+
+    public static void main(String[] args) {
+        Library library = new Library();
+        // System.out.println("The name of the book is " + library.bookName);
+        library.bookDetails();
+        library.updateQuantity(4);
+        System.out.println("Book count for " + library.bookName + " is " + library.quantity);
+    }
+
+}
